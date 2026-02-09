b387e72a5481546ed6049d465cd672d9c472233f
chocho77 <chavmom@gmail.com>
2026-02-09 21:28:39 +0200

    new file:   .gitignore 	new file:   src/module-info.java 	new file:   src/source/Hello.java 	new file:   src/source/MyClass.java

---
+++ src/source/Hello.java
@@ -0,0 +1,14 @@
+package source;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		MyClass myClass = new MyClass();
+		int num = myClass.a;
+		System.out.println(num);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/source/MyClass.java
@@ -0,0 +1,6 @@
+package source;
+
+public class MyClass {
+	public int a = 5;
+
+}
